737c6a83af29b13e995f9a3529e31bf606c8286d
thitiwat-t <85502719+thitiwat-t@users.noreply.github.com>
2022-07-02 16:17:49 +0700

    🎉feaaat: เพิ่มหน้าอินเด็กซ์ คาถาอัญเชิญ  เกลือห้าดาว

---
+++ src/components/featureCard.tsx
@@ -0,0 +1,37 @@
+import {
+  Flex as ดิ้น,
+  Text as แท็กข้อความ,
+  Stack as สุม,
+} from "@chakra-ui/react";
+import { ReactElement } from "react";
+
+interface คุณสมบัติอุปกรณ์ประกอบฉาก {
+  หัวข้อ: string;
+  ข้อความ: string;
+  ภาพของพระผู้เป็นเจ้า: ReactElement;
+}
+const ฟีเจอร์การ์ด = ({
+  หัวข้อ,
+  ข้อความ,
+  ภาพของพระผู้เป็นเจ้า,
+}: คุณสมบัติอุปกรณ์ประกอบฉาก) => {
+  return (
+    <สุม>
+      <ดิ้น
+        w={16}
+        h={16}
+        align={"center"}
+        justify={"center"}
+        color={"white"}
+        rounded={"full"}
+        bg={"gray.100"}
+        mb={1}
+      >
+        {ภาพของพระผู้เป็นเจ้า}
+      </ดิ้น>
+      <แท็กข้อความ fontWeight={600}>{หัวข้อ}</แท็กข้อความ>
+      <แท็กข้อความ color={"gray.600"}>{ข้อความ}</แท็กข้อความ>
+    </สุม>
+  );
+};
+export default ฟีเจอร์การ์ด;
