d975ecd912e811c77e7746a45228d996b62816ed
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-07-02 22:17:32 +0700

    Create 3D model

---
+++ src/components/model.tsx
@@ -0,0 +1,50 @@
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import {
+  Environment,
+  OrbitControls,
+  Html,
+  useProgress,
+} from "@react-three/drei";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
+import { DDSLoader } from "three-stdlib";
+import { Suspense } from "react";
+
+THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
+
+const Scene = () => {
+  //   const materials = useLoader(MTLLoader, "Poimandres.mtl");
+  const obj = useLoader(OBJLoader, "models/rr.obj", (loader) => {
+    // materials.preload();
+    // loader.setMaterials(materials);
+  });
+
+  console.log(obj);
+  return <primitive object={obj} scale={30} />;
+};
+
+const Loader = () => {
+  const { progress } = useProgress();
+  return (
+    <Html center>{progress === 100 ? <></> : <>{progress} % loaded</>}</Html>
+  );
+};
+
+export default function Model() {
+  return (
+    <>
+      <Canvas style={{ height: "50vh" }}>
+        <Loader />
+        {/* <ambientLight /> */}
+        <pointLight position={[10, 10, 10]} />
+        <Suspense fallback={null}>
+          <Scene />
+          <OrbitControls autoRotate={true} autoRotateSpeed={10} />
+          {/* <Environment preset="sunset" background /> */}
+        </Suspense>
+      </Canvas>
+    </>
+  );
+}
---
+++ src/pages/test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import Box from "@components/box";
+import Model from "@components/model";
+
+type Props = {};
+
+const test = (props: Props) => {
+  return (
+    <>
+      <Model />
+    </>
+  );
+};
+
+export default test;
