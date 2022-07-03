992fab05e97810d7f973f6223a4e08de32d26b8a
thitiwat-t <85502719+thitiwat-t@users.noreply.github.com>
2022-07-03 09:59:53 +0700

    🔧fix: พิมพ์สคริปอะไรซักอย่าง โว้วโว

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
-export { default as Model } from "./Model";
+export { default as โมเด็ล } from "./Model";
 export { default as การ์ดฟีเจอร์ } from "./FeatureCard";
 export { default as การ์ดสินค้า } from "./ProductCard";
+export { default as ดาวน์ซินโดรมโมดอลล์ } from "./DownloadModal";
